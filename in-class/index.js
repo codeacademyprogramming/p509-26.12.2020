ce8b7ac0a6034295658588d9dd1704306435f258
Ramil Mamedov <74110914+ramilmamedoff@users.noreply.github.com>
2020-12-26 17:45:57 +0400

    Add files via upload

---
+++ in-class/index.js
@@ -0,0 +1,39 @@
+const documentExample = {
+	html: {
+		head: {
+			meta: {
+				charset: "UTF-8",
+				viewport: "width=device...",
+			},
+			title: {
+				text: {
+					value: "Code Academy"
+				},
+			},
+		},
+		body: {
+			div: {
+				h1: {
+
+				}
+			}
+		},
+	},
+};
+
+const ccsom = {
+	body : {
+		"background-color": "red",
+		"font-size": "16px",
+	}
+}
+
+
+class Animal {}
+
+class Birds extends Animal {}
+
+class Chiken extends Birds {
+
+}
+
